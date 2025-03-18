b53901b1f3491b83638b3c11e722ecb8ef31f504
Elizabeth <ziv6484@gmail.com>
2025-03-18 16:43:43 +0200

    add main for testing

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
 # This is a sample Python script.
+import threading
+import time
+import schedule
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from engine.smart_garden_engine import SmartGardenEngine
 
+def run():
+    garden_engine  = SmartGardenEngine()
+    print("🌱 Smart Garden system is running!\n")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    garden_engine.add_plant(1, sensor_id=101, desired_moisture=70, pot_size="Medium",
+                            schedule_data=[{"day": "Tuesday", "time": "14:02", "duration": 10}])
+
+    garden_engine.add_plant(2, sensor_id=102, desired_moisture=50, pot_size="Large",
+                            schedule_data=[{"day": "Tuesday", "time": "14:02", "duration": 15}])
+
+    moisture_thread = threading.Thread(target=garden_engine.start_moisture_monitoring, daemon=True)
+
+    def run_schedule():
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+
+    schedule_thread = threading.Thread(target=run_schedule, daemon=True)
+
+    moisture_thread.start()
+    schedule_thread.start()
+
+    try:
+        while True:
+            time.sleep(10)
+    except KeyboardInterrupt:
+        print("\n❌ Exiting Smart Garden system...")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    run()
 
