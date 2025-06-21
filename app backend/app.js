1d6eedc034f303d389fe98330ad13ccc437b57b7
Elizabeth <ziv6484@gmail.com>
2025-06-21 18:00:52 +0300

    adding connection to database
    Signed-off-by: Elizabeth <ziv6484@gmail.com>

--- app backend/app.js
+++ app backend/app.js
@@ -1,8 +1,12 @@
 const WebSocket = require('ws');
 const { handleSocketConnection } = require('./sockets/index');
 const { sendSuccess, sendError } = require('./utils/wsResponses');
+const { testConnection } = require('./config/database'); 
+// Test the database connection at startup
 const wss = new WebSocket.Server({ port: 8080 });
+testConnection();
 
+// Handle incoming WebSocket connections
 wss.on('connection', (ws) => {
   ws.once('message', (msg) => {
     let data;
