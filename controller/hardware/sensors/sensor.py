320624459013871803977bb7b618bda2119c9338
Elizabeth <ziv6484@gmail.com>
2025-08-04 16:49:59 +0300

    a

--- controller/hardware/sensors/sensor.py
+++ controller/hardware/sensors/sensor.py
@@ -84,7 +84,7 @@ class Sensor:
                 # Read two registers starting from address 1 (matching mbpoll command)
                 # Try without unit parameter first
                 result = await modbus_client.read_input_registers(
-                    address=1,
+                    address=0,
                     count=2
                 )
 
