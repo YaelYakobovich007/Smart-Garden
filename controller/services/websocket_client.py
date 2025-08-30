35d275bd796ea6695cd49bf72819c4139e69b71c
YaelYakobovich007 <yaely7888@gmail.com>
2025-08-30 15:58:48 +0300

    change server url to local in the controller
    Signed-off-by: YaelYakobovich007 <yaely7888@gmail.com>

--- controller/services/websocket_client.py
+++ controller/services/websocket_client.py
@@ -13,9 +13,10 @@ class SmartGardenPiClient:
     Handles only essential functionality: connection, add plant, and moisture requests.
     """
 
+#str = "wss://smart-garden-backend-1088783109508.europe-west1.run.app"
 
     # For production, use Cloud Run URL
-    def __init__(self, server_url: str = "wss://smart-garden-backend-1088783109508.europe-west1.run.app", 
+    def __init__(self, server_url: str = "ws://192.168.68.74:8080", 
                  family_code: str = None, engine: SmartGardenEngine = None):
         self.server_url = server_url
         self.websocket: Optional[websockets.WebSocketServerProtocol] = None
